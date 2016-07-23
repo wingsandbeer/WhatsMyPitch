380fe1b165c9b4a5d82dafb3304a86493126c6a1
Gunhan Gulsoy <gunhangulsoy@gmail.com>
2016-07-23 15:34:21 -0700

    Initial commit

---
+++ app/src/main/java/com/wingsandbeer/whatsmypitch/Tuner.java
@@ -0,0 +1,13 @@
+package com.wingsandbeer.whatsmypitch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tuner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tuner);
+    }
+}
